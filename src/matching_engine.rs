4c320115c11218713bfdc86c810ae5f8d7e1a11d
quintoorschot <quintvanoorschot@gmail.com>
2026-08-15 14:39:46 +0200

    Introduce Trade struct for future trade processing

--- src/matching_engine.rs
+++ src/matching_engine.rs
@@ -1,5 +1,9 @@
-
-
 pub struct MatchingEngine {
     orderbook: OrderBook,
 }
+
+// impl MatchingEngine {
+
+//     fn submit_order(price: i64, quantity: u64) -> 
+
+// }
---
+++ src/trade.rs
@@ -0,0 +1,4 @@
+pub struct Trade {
+    price: i64,
+    quantity: u64
+}
