957d0ead04b647c820d4af72b2854b2159150d67
quintoorschot <quintvanoorschot@gmail.com>
2026-08-22 13:10:28 +0200

    Add doc comment for Order struct

--- src/order.rs
+++ src/order.rs
@@ -1,16 +1,30 @@
 use core::fmt;
 
+/// Represents the side (buy/sell) of an order.
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Side {
     Buy,
     Sell,
 }
 
+/// Represents an instruction to buy or sell an asset.
+///
+/// - `id` (u64): identifier for the order.
+/// - `price` (i64): price in ticks (e.g. 10050 = $100.50) at which the order is willing to execute.
+/// - `quantity` (u64): number of units the order is willing to buy or sell.
+///  - `side` (Side): indicates whether the order is a buy or sell.
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Order {
+    /// Identifier for the order.
     pub id: u64,
-    pub price: i64, // Price in ticks (e.g. 10050 = $100.50)
+
+    /// Price in ticks (e.g. 10050 = $100.50) at which the order is willing to execute.
+    pub price: i64,
+
+    /// Number of units the order is willing to buy or sell.
     pub quantity: u64,
+
+    /// indicates whether the order is a buy or sell.
     pub side: Side,
 }
 
