e90c7f2afb24f0a0ab2bc526351fc4e6f3c1fc3f
quintoorschot <quintvanoorschot@gmail.com>
2026-08-27 22:29:38 +0200

    Add doc strings to clock struct and its methods

--- src/simulator/clock.rs
+++ src/simulator/clock.rs
@@ -1,13 +1,16 @@
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
 pub struct SimTime(pub u64);
 
+/// Represents simulated time in discrete-event simulation.
 impl SimTime {
     pub const ZERO: Self = Self(0);
 
+    /// Returns the currently simulated time in nano seconds.
     pub fn nanos(&self) -> u64 {
         self.0
     }
 
+    /// Add nano seconds to current simulated time.
     pub fn add_nanos(self, nanos: u64) -> Self {
         Self(self.0.checked_add(nanos).expect("Simulation time overflow"))
     }
